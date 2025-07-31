405bfb61de9d0bd8907ab8b8aff6a6c74fef8285
mengjunsu <mengjunsu1@gmail.com>
2025-07-31 15:58:47 +0800

    feat: add boilerplate.txt for adding license automatically and add a LICENSE file

--- cmd/miniblog/main.go
+++ cmd/miniblog/main.go
@@ -1,7 +1,12 @@
+// Copyright 2025 Innkeeper DanteSu(苏孟君) <mengjunsu@163.com>. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file. The original repo for
+// this file is https://github.com/DanteSu/miniblog
+
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("Hello miniblog!")
+	fmt.Println("Hello 123miniblog1231!777")
 }
