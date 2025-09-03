32393d2729f256e009fe68714599278179f3c26f
mengjunsu <mengjunsu1@gmail.com>
2025-09-03 16:22:08 +0800

    feat:  github.com/Shelnutt2/db2struct/cmd/db2struct@latest create model

---
+++ internal/pkg/model/post.go
@@ -0,0 +1,16 @@
+package model
+
+type PostM struct {
+	Content   string    `gorm:"column:content"`        //
+	CreatedAt time.Time `gorm:"column:createdAt"`      //
+	ID        int64     `gorm:"column:id;primary_key"` //
+	PostID    string    `gorm:"column:postID"`         //
+	Title     string    `gorm:"column:title"`          //
+	UpdatedAt time.Time `gorm:"column:updatedAt"`      //
+	Username  string    `gorm:"column:username"`       //
+}
+
+// TableName sets the insert table name for this struct type
+func (p *PostM) TableName() string {
+	return "post"
+}
---
+++ internal/pkg/model/user.go
@@ -0,0 +1,17 @@
+package model
+
+type UserM struct {
+	CreatedAt time.Time `gorm:"column:createdAt"`      //
+	Email     string    `gorm:"column:email"`          //
+	ID        int64     `gorm:"column:id;primary_key"` //
+	Nickname  string    `gorm:"column:nickname"`       //
+	Password  string    `gorm:"column:password"`       //
+	Phone     string    `gorm:"column:phone"`          //
+	UpdatedAt time.Time `gorm:"column:updatedAt"`      //
+	Username  string    `gorm:"column:username"`       //
+}
+
+// TableName sets the insert table name for this struct type
+func (u *UserM) TableName() string {
+	return "user"
+}
